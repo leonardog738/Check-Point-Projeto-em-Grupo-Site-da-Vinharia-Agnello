cc78d1e83df3830c2da07301e7a0e3e0a6025d15
leonardog738 <leogcfonseca@gmail.com>
2026-04-05 21:48:34 -0300

    ajustes de filtro

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const searchInput = document.getElementById("pesquisa");
   const filtroCategoria = document.getElementById("tipo");
   const produtos = document.querySelectorAll(".produto");
+  const notFoundArea = document.getElementById("not-found"); 
 
-  // função que remove acentos
   function normalizar(texto) {
     return texto
       .toLowerCase()
@@ -14,10 +14,11 @@ document.addEventListener("DOMContentLoaded", () => {
   function filtrarProdutos() {
     const busca = normalizar(searchInput.value);
     const categoria = normalizar(filtroCategoria.value);
+    
+    let produtosVisiveis = 0; 
 
     produtos.forEach((produto) => {
       const textoProduto = normalizar(produto.innerText);
-
       let tipo = "";
 
       if (textoProduto.includes("tinto")) tipo = "tinto";
@@ -30,11 +31,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
       if (matchBusca && matchCategoria) {
         produto.style.display = "flex";
+        produtosVisiveis++;
       } else {
         produto.style.display = "none";
       }
     });
+
+    
+    if (produtosVisiveis === 0) {
+      notFoundArea.style.display = "block";
+    } else {
+      notFoundArea.style.display = "none";
+    }
   }
+
+ 
+  window.limparFiltros = () => {
+    searchInput.value = "";
+    filtroCategoria.value = "";
+    filtrarProdutos(); 
+  };
 
   searchInput.addEventListener("input", filtrarProdutos);
   filtroCategoria.addEventListener("change", filtrarProdutos);
